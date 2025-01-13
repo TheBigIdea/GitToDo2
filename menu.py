66498ae9acb4ad8cf60d4ea28bdf155bf99dab95
Suraj Mishra <mishrasur7@gmail.com>
2025-01-13 13:14:32 +0200

    main menu design added

---
+++ menu.py
@@ -0,0 +1,19 @@
+print('Welcome to simple task manager!\n')
+
+print('Press 1 to add task to the list.')
+print('Press 2 to view tasks.')
+print('Press 3 to remove task from the list.')
+print('Press 4 to exit from the program.')
+
+def main(num):
+    match num:
+        case 1:
+            pass
+        case 2:
+            pass
+        case 3:
+            pass
+        case 4:
+            pass
+        case _:
+            print('Out of range instructions. Goodbye!')
