49d4c22b609f2f11e1db1bbe43f3cc5c334f6fd8
Suraj Mishra <mishrasur7@gmail.com>
2025-01-13 14:18:35 +0200

    loop implemented

--- main.py
+++ main.py
@@ -8,17 +8,18 @@ print('Press 2 to view tasks.')
 print('Press 3 to remove task from the list.')
 print('Press 4 to exit from the program.')
 
-def main(num):
-    match num:
-        case 1:
-            todo.add_task()
-        case 2:
-            todo.view_tasks(todo.tasks)
-        case 3:
-            todo.remove_task()
-        case 4:
-            print('Good bye!')
-        case _:
-            print('Out of range instructions. Goodbye!')
-
-main(int(input('Please enter your choice: ')))
+while True:
+    def main(num):
+        match num:
+            case 1:
+                todo.add_task()
+            case 2:
+                todo.view_tasks(todo.tasks)
+            case 3:
+                todo.remove_task()
+            case 4:
+                print('Goodbye!')
+            case _:
+                print('Out of range instructions. Goodbye!')
+    
+    main(int(input('Please enter your choice: ')))
