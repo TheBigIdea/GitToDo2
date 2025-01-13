0f95a8ce125199f0fcddf81f3295a29ef4b2e972
Juho Ahopelto <ja033417@tite.amiedu-ict.net>
2025-01-13 13:29:12 +0200

    remove_task funktio muutettu

--- todo.py
+++ todo.py
@@ -1,17 +1,10 @@
-
-
-def remove_task(tasks):
-    result = tasks.copy()
-    while True:
-        for index, task in enumerate(result):
-            print(index, task)
-        break
-    index_to_be_removed = int(input('anna poistettavan tehtävän numero: '))
-    if index_to_be_removed in range(len(result)):
-        result.pop(index_to_be_removed)
+def remove_task():
+    view_tasks(tasks)
+    index_to_be_removed = int(input('g: '))
+    if index_to_be_removed in range(len(tasks)):
+        tasks.pop(index_to_be_removed)
     else:
         print('mitään ei poistettu')
-    return result
 
 
 tasks = []
